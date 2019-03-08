cd89a8aaaf91dba72e403e16595a26f323825bf0
jFriedli <jonas.friedli@hotmail.com>
2019-03-08 14:47:58 +0100

    add entity deletion with cascading types

---
+++ src/main/java/ch/jfriedli/hibernate/demo/DeleteDemo.java
@@ -0,0 +1,49 @@
+package ch.jfriedli.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import ch.jfriedli.hibernate.demo.entity.Instructor;
+import ch.jfriedli.hibernate.demo.entity.InstructorDetail;
+import ch.jfriedli.hibernate.demo.entity.Student;
+
+public class DeleteDemo {
+
+	public static void main(String[] args) {
+
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.buildSessionFactory();
+
+		Session session = factory.getCurrentSession();
+
+		try {
+			session.beginTransaction();
+			
+			int id = 1;
+			
+			Instructor instructor = session.get(Instructor.class, id);
+			
+			System.out.println("Instructor: " + instructor);
+			
+			if (instructor != null) {
+				System.out.println("Deleting instructor");
+				session.delete(instructor);
+			}
+
+			session.getTransaction().commit();
+
+			System.out.println("Done!");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			factory.close();
+		}
+
+	}
+
+}
