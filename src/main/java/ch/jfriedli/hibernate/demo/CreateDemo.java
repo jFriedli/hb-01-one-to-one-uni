5ac2ac6318c04c923d08151171ec77ec695a38de
jFriedli <jonas.friedli@hotmail.com>
2019-03-08 14:42:12 +0100

    save entities to DB using cascade

---
+++ src/main/java/ch/jfriedli/hibernate/demo/CreateDemo.java
@@ -0,0 +1,47 @@
+package ch.jfriedli.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import ch.jfriedli.hibernate.demo.entity.Instructor;
+import ch.jfriedli.hibernate.demo.entity.InstructorDetail;
+import ch.jfriedli.hibernate.demo.entity.Student;
+
+public class CreateDemo {
+
+	public static void main(String[] args) {
+
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.buildSessionFactory();
+
+		Session session = factory.getCurrentSession();
+
+		try {
+			Instructor instructor = new Instructor("Jonas", "Friedli", "jonas.friedli@example.com");
+			
+			InstructorDetail instructorDetail = new InstructorDetail("jfriedli.com", "Spring configuration");
+			
+			instructor.setInstructorDetail(instructorDetail);
+			
+			session.beginTransaction();
+
+			//also saves the instructorDetail object because of CascadeType.ALL
+			session.save(instructor);
+			
+			session.getTransaction().commit();
+
+			System.out.println("Done!");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			factory.close();
+		}
+
+	}
+
+}
